c2e8fc32a387c119554ad7f9127848b1e800efb5
Teon Ooi <teon@protonmail.com>
2023-07-31 22:09:31 +0800

    Update with skeleton layout.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,33 @@
 </script>
 
 <template>
+    <div class="row mb-3">
+        <div class="col col-xl-8 offset-xl-2">
 
+            <h1>Todo</h1>
+
+        </div>
+    </div>
+
+    <div class="row d-flex justify-content-center">
+        <div class="col col-xl-8">
+
+            <div class="card">
+                <div class="card-body p-5">
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mt-3">
+        <div class="col col-xl-8 offset-xl-2 text-end">
+
+            <span>Made with <a href="https://vuejs.org" target="_blank">Vue.js</a></span>
+
+        </div>
+    </div>
 </template>
 
 <style scoped>
